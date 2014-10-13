eb0b32921d75f75c307caf2937fea5441cbcfec6
alexminnaar <minnaaralex@gmail.com>
2014-10-13 14:12:08 -0400

    some style clean-up

--- src/test/scala/IntegratedSpec.scala
+++ src/test/scala/IntegratedSpec.scala
@@ -1,26 +1,19 @@
 import com.topic.models.corpus.StreamingCorpus
 import com.topic.models.models.OnlineLDA
 import com.topic.models.vocabulary.CountVocab
-import scala.io.Source
-import java.io.File
 
-/**
- * Created by alex on 21/09/14.
- */
+
 object IntegratedSpec extends App {
 
-  val docDirectory="NIPS_dataset/"
+  val docDirectory = "NIPS_dataset/"
 
-  val testVocab=CountVocab(docDirectory,10).getVocabulary
+  val testVocab = CountVocab(docDirectory, 10).getVocabulary
 
-  val testCorpus=new StreamingCorpus(testVocab,5,docDirectory)
+  val testCorpus = new StreamingCorpus(testVocab, 5, docDirectory)
 
-  println(testCorpus.batchFileList)
+  val oldaTest = new OnlineLDA(testCorpus, 5, 0.5, 500)
 
-  println(testCorpus.getNextMiniBatch)
-  val oldaTest=new OnlineLDA(testCorpus,5,0.5,500)
   oldaTest.inference()
+
   oldaTest.printTopics(10)
-
-
 }
